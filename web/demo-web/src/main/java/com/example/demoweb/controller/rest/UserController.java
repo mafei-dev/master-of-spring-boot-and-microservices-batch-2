b3a4f9e0085c024e1d41b7eaeacb06f6dbade9b6
mafei <mafei.dev@gmail.com>
2023-02-26 21:21:04 +0530

    shifted to specific mapping annotations

--- web/demo-web/src/main/java/com/example/demoweb/controller/rest/UserController.java
+++ web/demo-web/src/main/java/com/example/demoweb/controller/rest/UserController.java
@@ -9,28 +9,33 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     //user/{username}
-    @RequestMapping(path = "/{username}", method = {RequestMethod.GET})
+//    @RequestMapping(path = "/{username}", method = {RequestMethod.GET})
+    @GetMapping(path = "/{username}")
     public DB.User getUser(@PathVariable("username") String username) {
         //process
         return DB.getUser(username);
     }
 
 
-    @RequestMapping(method = RequestMethod.POST)
+    //    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public void addUser(@RequestBody DB.User user) {
         System.out.println("user = " + user);
         //process
         DB.addNewUser(user);
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
+
+//    @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public void updateUser(@RequestBody DB.User oldUser) {
         //process
         DB.updateUser(oldUser);
     }
 
 
-    @RequestMapping(method = RequestMethod.DELETE)
+//    @RequestMapping(method = RequestMethod.DELETE)
+    @DeleteMapping
     public void deleteUser(
             @RequestParam("username") String username,
             @RequestParam(value = "isActive") Boolean isActive
