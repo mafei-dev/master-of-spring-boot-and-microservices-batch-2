47b579a49569ad96b9dc420133a7fa639a3b8551
mafei <mafei.dev@gmail.com>
2023-02-26 21:14:48 +0530

    shifted to @RestController

--- web/demo-web/src/main/java/com/example/demoweb/controller/rest/UserController.java
+++ web/demo-web/src/main/java/com/example/demoweb/controller/rest/UserController.java
@@ -1,16 +1,15 @@
 package com.example.demoweb.controller.rest;
 
 import com.example.demoweb.db.DB;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(path = "/user")
+//REST
 public class UserController {
 
     //user/{username}
     @RequestMapping(path = "/{username}", method = {RequestMethod.GET})
-    @ResponseBody//application/json; charset=utf-8 | REST
     public DB.User getUser(@PathVariable("username") String username) {
         //process
         return DB.getUser(username);
@@ -18,7 +17,6 @@ public class UserController {
 
 
     @RequestMapping(method = RequestMethod.POST)
-    @ResponseBody//application/json; charset=utf-8 | REST
     public void addUser(@RequestBody DB.User user) {
         System.out.println("user = " + user);
         //process
@@ -26,7 +24,6 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.PUT)
-    @ResponseBody//application/json; charset=utf-8 | REST
     public void updateUser(@RequestBody DB.User oldUser) {
         //process
         DB.updateUser(oldUser);
@@ -34,7 +31,6 @@ public class UserController {
 
 
     @RequestMapping(method = RequestMethod.DELETE)
-    @ResponseBody//application/json; charset=utf-8 | REST
     public void deleteUser(
             @RequestParam("username") String username,
             @RequestParam(value = "isActive") Boolean isActive
