91555dcd21c03b1142028767b6317180acb488a0
mafei <mafei.dev@gmail.com>
2023-03-27 02:27:36 +0530

    microservices init

---
+++ web/demo-web/order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.orderservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
---
+++ web/demo-web/order-service/src/main/java/com/example/orderservice/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public Object getDate() {
+        return LocalDateTime.now();
+    }
+}
